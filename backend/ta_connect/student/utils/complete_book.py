504ec32b343b8c9d113c9f706fe7ceed2f9279b1
Karim Bassem Joseph <k.bassem2397@nu.edu.eg>
2025-12-06 18:37:48 +0200

    Enhance booking management with timezone handling and completion logic

--- backend/ta_connect/student/utils/complete_book.py
+++ backend/ta_connect/student/utils/complete_book.py
@@ -2,34 +2,6 @@ from datetime import datetime
 from django.utils import timezone
 
 def complete_booking(booking):
-    """
-    Marks a booking as completed.
-    Compares booking end time with current time in the same timezone.
-    """
-    now_utc = timezone.now()
-    
-    booking_end = booking.end_time
-
-    print(f"Current UTC time: {now_utc}, Booking end time: {booking_end}")
-
-    if not booking or booking.status not in ["confirmed", "pending"]:
-        return False, "Invalid or cancelled booking."
-    
-    if not booking_end:
-        return False, "Booking end time is not set."
-
-    if booking.end_time >= now_utc:
-         return False, "Booking has not ended yet."
-    
-    try:
-        if booking.status == "confirmed":
-            booking.complete()
-            booking.save()
-            return True, "Booking marked as completed."
-        else:
-            booking.cancel()
-            booking.save()
-            return True, "Booking was pending and is now cancelled."
-    except Exception as e:
-        print(f"Error completing booking: {e}")
-        return False, "something went wrong while completing the booking."
+    success, message = booking.complete_if_ended()
+    print(message)
+    return success, message
